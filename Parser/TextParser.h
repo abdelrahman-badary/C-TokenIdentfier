3fc6424f0f8c8aed59b0d7703272d3cb0de1f33e
zetta_ <zetta_fci@hotmail.com>
2015-10-22 14:51:47 +0200

    Added readme

---
+++ Parser/TextParser.h
@@ -0,0 +1,13 @@
+//
+//  TextParser.h
+//  Parser
+//
+//  Created by Abd al rhman Taher on 10/22/15.
+//  Copyright © 2015 Abd al rhman Taher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextParser : NSObject
+
+@end
---
+++ Parser/TextParser.m
@@ -0,0 +1,13 @@
+//
+//  TextParser.m
+//  Parser
+//
+//  Created by Abd al rhman Taher on 10/22/15.
+//  Copyright © 2015 Abd al rhman Taher. All rights reserved.
+//
+
+#import "TextParser.h"
+
+@implementation TextParser
+
+@end
